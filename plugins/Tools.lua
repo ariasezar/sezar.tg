36f3bb401c5fccc6ecb7ef2d0d5aa5059ae7e685
ariasezar <asghar.ghasemi.96@gmail.com>
2017-04-11 15:02:21 +0430

    aria

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 304240834 -- put Your ID here! <===
+local SUDO = 112617435 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
